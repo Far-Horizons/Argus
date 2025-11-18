829dd39a6feecd7ab6578fec45feb96642d77bc0
Far-Horizons <lars.van.mil.05@gmail.com>
2025-11-18 16:53:06 +0100

    Update Argus to version 0.1.3

--- ArgusPythonFiles/__main__.py
+++ ArgusPythonFiles/__main__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+import os
+import sys
 
 import StartManager
 
 if __name__ == "__main__":
-    starter = StartManager.StartManager()
-    starter.start()
+    correct_directory = os.path.expanduser("~/Argus")
+    current_directory = os.getcwd()
+
+    if current_directory == correct_directory:
+        starter = StartManager.StartManager()
+        starter.start()
+    else: 
+        print(f"Please run Argus from the {correct_directory} directory.")
+        sys.exit(1)
